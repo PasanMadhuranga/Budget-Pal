db3de2bf492050508bfb80e9f092267fa835c035
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-07-02 16:09:02 +0530

    initialize some other screens

---
+++ budget-pal-frontend/app/screens/SignupScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignUpScreen(props) {
+  return <div></div>;
+}
+
+export default SignUpScreen;
--- budget-pal-frontend/app/screens/WelconeScreen.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function WelconeScreen(props) {
-  return <div></div>;
-}
-
-export default WelconeScreen;
